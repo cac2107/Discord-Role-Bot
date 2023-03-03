00ca81deb48526d03f10bf98e74c5b2a100c3553
cac2107 <cac2106@rit.edu>
2023-03-03 13:24:15 -0500

    JSON Refactoring

--- data/dataFilesCreate.js
+++ data/dataFilesCreate.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
-let groups = {"guilds": []};
-let menus = {"guilds": []};
+let groups = {"guilds": {}};
+let menus = {"guilds": {}};
 
 fs.writeFileSync('./data/groups.json', JSON.stringify(groups, null, 2), (err) => { console.log(err); });
 fs.writeFileSync('./data/menus.json', JSON.stringify(menus, null, 2), (err) => { console.log(err); });
