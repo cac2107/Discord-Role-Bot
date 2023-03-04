6b6ed5530493e95b4e3fdb4387e3aee4875bd8cd
cac2107 <cac2106@rit.edu>
2023-03-04 17:14:41 -0500

    Refactored to use channelOption

--- commands/setAdminChannel.js
+++ commands/setAdminChannel.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, ChannelType } = require('discord.js');
 const menus = require("../data/menus.json");
 const fs = require('fs');
 
@@ -6,20 +6,14 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('setadminchannel')
 		.setDescription('Sets the channel to send menu interaction information to.')
-        .addStringOption(option =>
+        .addChannelOption(option =>
             option.setName('channel')
                 .setDescription('#channel to send the information to. Please use #channel format')
+                .addChannelTypes(ChannelType.GuildText)
                 .setRequired(true)),
 	async execute(interaction) {
-        let channel = interaction.options.getString('channel');
+        let channel = interaction.options.getChannel('channel').id;
         let guildId = interaction.guild.id;
-
-        if(!(channel.startsWith("<#"))){
-            await interaction.reply({content: "Sorry, that is not a channel. Please use #channel format."})
-            return;
-        }
-
-        channel = channel.replace("<#", "").replace(">", "");
 
         if(menus.guilds[guildId] == undefined){ menus.guilds[guildId] = {}; }
 
